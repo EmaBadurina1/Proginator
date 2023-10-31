d2d3fce862028f427b44393d2a3622368d42daf0
Fran Zlatar <fran.zlatar@fer.hr>
2023-10-31 01:47:32 +0100

    Dodan folder src/components

---
+++ IzvorniKod/frontend/src/components/Button.js
@@ -0,0 +1,9 @@
+import "Button.css";
+
+const Button = () => {
+   return (
+      <button>Button</button>
+   );
+}
+ 
+export default Button;
--- IzvorniKod/frontend/src/pages/Home.js
+++ IzvorniKod/frontend/src/pages/Home.js
@@ -1,3 +1,5 @@
+import "Home.css";
+
 const Home = () => {
    return (
       <div className="home">
