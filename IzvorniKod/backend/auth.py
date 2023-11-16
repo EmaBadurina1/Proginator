ad731258eebb5784bf45cfb31a02084ba2840f4c
KarloVrd <karlo.vrdoljak@gmail.com>
2023-11-16 21:13:10 +0100

    better backend structure
    added to .gitignore
    
    removed .env
    
    added empty .env
    
    Revert "added empty .env"
    
    This reverts commit 7feb45b57c681fe1824c7c69fcaa25ec93c00e9b.
    
    Revert "Revert "added empty .env""
    
    This reverts commit 610af0b559c29d1d8d53c73f2e4c2ee3eb77ebc8.
    
    removed .env
    
    giga changes
    
    removed venv, added controllers folder, removed .env bcs security risks

--- IzvorniKod/backend/auth.py
+++ IzvorniKod/backend/auth.py
@@ -1,63 +1,20 @@
 import jwt
 from datetime import datetime, timedelta
-import config
 from flask import abort, jsonify
 from functools import wraps
 from flask import request, session
 from flask import current_app as app
 
-def generate_token(user_id):
-    expiration_time = datetime.utcnow() + timedelta(days=1)  # Token expiration time
-    payload = {
-        'user_id': user_id,
-        'exp': expiration_time
-    }
-    token = jwt.encode(payload, config.SECRET_KEY, algorithm='HS256')
-    return token
 
-def validate_token(token):
-    try:
-        payload = jwt.decode(token, config.SECRET_KEY, algorithms=['HS256'])
-
-        if not payload:
-            invalid_token_response = jsonify({'error': 'Token is invalid'})
-            invalid_token_response.status_code = 401
-            abort(invalid_token_response)
-
-        return payload
-    
-    except jwt.ExpiredSignatureError:
-        expired_token_response = jsonify({'error': 'Token has expired'})
-        expired_token_response.status_code = 401
-        abort(expired_token_response)
-
-    except jwt.InvalidTokenError:
-        invalid_token_response = jsonify({'error': 'Token is invalid'})
-        invalid_token_response.status_code = 401
-        abort(invalid_token_response)
-
-
-def auth(f):
+def auth_validation(f):
     '''
-    Decorator that checks if a token is provided in the request header.
-    If a token is provided, it is validated and the user_id is extracted from it.
-    The user_id is then passed to the decorated function as an first argument (auth_user_id).
+    Decorator that checks if the user is logged in.
     '''
     @wraps(f)
     def wrapper(*args, **kwargs):
-        if app.config['TESTING']:
-            auth_user_id = 1
-            return f(auth_user_id, *args, **kwargs)
-
-        token = session.get('Authorization')
-
-        if not token:
-            no_token_response = jsonify({'error': 'No token provided'})
-            no_token_response.status_code = 401
-            return abort(no_token_response)
-        
-        payload = validate_token(token)
-
-        auth_user_id = payload['user_id']
-        return f(auth_user_id, *args, **kwargs)
+        if "user_id" not in session:
+            response = jsonify({"message": "Missing authorization"})
+            response.status_code = 401
+            abort(response)
+        return f(*args, **kwargs)
     return wrapper
--- IzvorniKod/backend/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-SECRET_KEY = 'secret_key'
-TOKEN_EXPIRATION_TIME = 60 * 60 * 24  # 1 day
-DATABASE_URI = f'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'database.db')
