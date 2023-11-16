546d55a94ddfa85849ec760daa534827ff3efc21
Fran Zlatar <fran.zlatar@fer.hr>
2023-11-16 17:36:11 +0100

    Popravljeno spajanje na render postgres bazu

--- IzvorniKod/backend/app.py
+++ IzvorniKod/backend/app.py
@@ -18,14 +18,8 @@ CORS(app, supports_credentials=True)
 
 load_dotenv()
 
-DB_USERNAME = os.getenv("DB_USERNAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-
 # Construct the database URL
-DB_URL = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+DB_URL = os.getenv("DB_URL")
 
 app = Flask(__name__)
 migrate = Migrate()
