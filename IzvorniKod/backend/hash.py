769cc55519f3bd3adeae5b852593e011e9388bc7
Fran Zlatar <fran.zlatar@fer.hr>
2023-11-17 21:34:31 +0100

    update

--- IzvorniKod/backend/hash.py
+++ IzvorniKod/backend/hash.py
@@ -1,4 +1,8 @@
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 if __name__ == "__main__":
-   print(generate_password_hash('progi1234'))
+   password = 'progi1234'
+   hash = generate_password_hash(password)
+   print(hash)
+
+   print(check_password_hash(hash, password))
