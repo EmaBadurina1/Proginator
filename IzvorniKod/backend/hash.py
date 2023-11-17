b1f08a0818e596e720da6e5fc06db7bf2b810b15
Fran Zlatar <fran.zlatar@fer.hr>
2023-11-17 22:15:05 +0100

    test

--- IzvorniKod/backend/hash.py
+++ IzvorniKod/backend/hash.py
@@ -5,5 +5,6 @@ if __name__ == "__main__":
    print(salt)
    password = 'progi1234'
    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-   print(hashed_password.decode('utf-8'))
-   print(bcrypt.checkpw(password.encode('utf-8'), hashed_password))
+   hashed_password = "$2b$12$ZCOqpHV4LynVhCNuhxIryu1Tx8X4MeO69YiogZxj6MBEFvRfLdLD."
+   #print(hashed_password.decode('utf-8'))
+   print(bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8')))
