90374fb436c5713f3b1b0703954b992c960f0b11
KarloVrd <karlo.vrdoljak@gmail.com>
2023-11-17 18:27:23 +0100

    dodan modul u requirementse

--- IzvorniKod/backend/app.py
+++ IzvorniKod/backend/app.py
@@ -5,7 +5,7 @@ import os
 
 from controllers import accounts_bp, appointments_bp
 from models import *
-from database import db
+from db import db
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -16,11 +16,11 @@ app.register_blueprint(appointments_bp)
 if __name__ == '__main__':
     load_dotenv()
     DB_URL = os.getenv("DB_URL")
-    app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
     app.config["SQLALCHEMY_ECHO"] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning
     app.config["SQLALCHEMY_RECORD_QUERIES"] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 
     db.init_app(app)
     # migrate.init_app(app, db)
--- IzvorniKod/backend/db.py
+++ IzvorniKod/backend/db.py
@@ -1,4 +1,4 @@
 # database
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
-# db = SQLAlchemy()
+db = SQLAlchemy()
